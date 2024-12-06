dff4fe31e19f90218445b85f465dc3c20f223d93
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-06 15:33:07 +0530

    Update Home.py

--- Streamlit/Pages/Home.py
+++ Streamlit/Pages/Home.py
@@ -1,1 +1,8 @@
-print("hii")
+import streamlit as st
+
+# Let's begin to learn Streamlit: 
+st.title("Intro to Streamlit - Tutorial")
+st.write("Streamlit is a service which can help us create easy and fast paced python website in less time.")
+
+st.image("Streamlit/static/logo.png",width=1200)
+st.markdown("<h3 style='text-align: right;'> - By KARTIK HAJELA </n3>", unsafe_allow_html=True)
