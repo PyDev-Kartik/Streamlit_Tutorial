f924db03c8447e11c5e0b889fbacf4633407574f
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-07 17:15:59 +0530

    Update 5_5 Form Element.py

--- Streamlit/Pages/5_5 Form Element.py
+++ Streamlit/Pages/5_5 Form Element.py
@@ -285,3 +285,11 @@ st.code("""
         
 """)
 st.divider()
+st.subheader("Help :")
+col1_foot,col2_foot = st.columns([3,1])
+with col1_foot:
+        st.page_link("Help.py",label = "Click Here")
+with col2_foot:
+        st.page_link("6_6 Advance Form Element.py",label = ":green-background[Next page >]")
+st.divider()
+
