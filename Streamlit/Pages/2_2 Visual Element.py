d5d25efede3dc52036bc77bdf24aef4f267d30e5
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-08 00:24:04 +0530

    Update 2_2 Visual Element.py

--- Streamlit/Pages/2_2 Visual Element.py
+++ Streamlit/Pages/2_2 Visual Element.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 st.header("2. Visual Elements :")
 st.write("1. Buttons : Used to perform some set of function.")
@@ -23,7 +24,7 @@ st.write("For me it's in :")
 st.write(f""" :blue-background["{path}"]""")
 st.write("""•   But python does't supports this addressing. So we would have to solve it. By introducing double-backslash mechanism for error control""")
 st.write("We can modify our path as :")
-st.image("Streamlit/static/img_path.png")
+st.image("streamlit/static/img_path.png")
 
 st.code("""
 # Code to display image of a cat : 
@@ -33,19 +34,165 @@ st.image("C:\\Users\\Kartik\\Desktop\\KDocuments\\Programming\\3. Python program
         caption="Cat in office",width=200)
 """) 
 
-st.image("Streamlit/static/code.png",width=5000)
+st.image("streamlit/static/code.png",width=5000)
 
+st.write("### So this is one of the complex addressing methods,\n ### let's see a simpler one too : ")
+st.code("""st.image("streamlit/static/cat.jpg",caption="Cat in office",width=200)""") 
 # Code to display image of a cat :
-st.image("Streamlit/static/cat.jpg",caption="Cat in office",width=200)
+
+st.image("streamlit/static/cat.jpg",caption="Cat in office",width=200)
 
 
 st.divider()
 
+st.write("3. Audio : To display audio file.")
 
+st.code("""st.audio("streamlit/static/multimedia/phonk.mp3",
+          format="audio/mpeg", 
+          loop=True,
+          start_time=4,
+          end_time=25,
+          autoplay=True)""")
 
-st.write("3. name3 : Define")
-st.code("""#code""")
+st.audio("streamlit/static/multimedia/phonk.mp3",
+          format="audio/mpeg", 
+          loop=True,
+          start_time=4,
+          end_time=25,
+          autoplay=True)
 
+st.divider()
+
+st.write("4. Audio-Input : To record audio.")
+
+st.code("""voice2 = st.audio_input("Record a Voice note")""")
+
+voice2 = st.audio_input("Record a Voice note")
+st.write("Playing the recorded audio : ")
+st.code("st.audio(voice2)")
+st.audio(voice2)
+
+st.divider()
+
+st.write("4. Video : To display the video.")
+
+st.code("""st.video("streamlit/static/multimedia/phonk.mp4",
+         format = "video/mp4",
+         start_time = 4,
+         subtitles = None,
+         end_time = 25 ,
+         loop = False,
+         autoplay= False,
+         muted= False)
+""")
+
+st.video("static/multimedia/phonk.mp4", format = "video/mp4", start_time = 4, subtitles = None, end_time = 25 , loop = False, autoplay= False, muted= False)
+
+st.divider()
+
+st.write("5. Camera-Input : To access the camera and click photos, save and access them.")
+
+st.code("""
+access = st.checkbox("Provide camera access : ")
+if access:
+    photo = st.camera_input("Click photo")
+    if photo:
+        st.image(photo)
+""")
+
+access = st.checkbox("Provide camera access : ")
+if access:
+    photo = st.camera_input("Click photo")
+    if photo:
+        st.image(photo)
+
+st.divider()
+
+st.write("6. Toast : To display quick message.")
+st.code("""
+text = st.text_input("Write anything")
+if text : 
+    st.toast('You inputted data sucessfully!', icon='😍')""")
+
+text = st.text_input("Write any thing")
+if text : 
+    st.toast('You inputted data sucessfully!', icon='😍')
+
+st.divider()
+
+st.write("7. Balloon/Snow : To display ballons and snow as easter eggs.")
+
+st.code("""
+ch1 = st.checkbox("Ballon")
+ch2 = st.checkbox("Snow")
+if ch1:
+    st.balloons()
+if ch2:
+    st.snow()""")
+
+ch1 = st.checkbox("Ballon")
+ch2 = st.checkbox("Snow")
+if ch1:
+    st.balloons()
+if ch2:
+    st.snow()
+
+st.divider()
+
+st.write("8. Spinner : To spin a wheel just like a loader it will load till our process finishes.")
+st.caption("In the given example we have used time module to add extra time.")
+
+st.code("""
+with st.spinner('Wait for it...'):
+    time.sleep(5)
+st.success("Done!")""")
+
+st.video("Streamlit/static/multimedia/spinner.mp4",autoplay=True,loop=True)
+
+st.divider()
+
+st.write("9. Status : To display the current status of the process")
+st.write("If the process is 'going-on' it would be displayed as : :blue-background[spinner]")
+st.write("If the process is 'completed' it would be displayed as : :green[✓]")
+st.write("If the process is 'completed' it would be displayed as : ⚠️")
+
+st.code("""
+#Code:
+st.status("Running...",expanded=True,state='running')
+st.status("Completed",expanded=True,state="complete")
+st.status("error occured",expanded=True,state="error")""")
+
+st.status("Running...",expanded=True,state='running')
+st.status("Completed",expanded=True,state="complete")
+st.status("error occured",expanded=True,state="error")
+
+st.divider()
+
+st.write("10. Chats : To display and send messages")
+st.write("st.chat_input - to provide text-box to write message")
+st.write("""st.chat_message("user",avatar="user") - to display message and avatar is used to signify who is writing message""")
+st.caption("There are just two modes of avatar : user and assistant")
+
+st.code('''
+prompt = st.chat_input("Say, Hii")
+if prompt:
+    with st.chat_message("user",avatar="user"):
+        st.write(prompt)
+    with st.chat_message("AI",avatar="assistant"):
+        st.write("Hello 👋")
+''')
+st.write("## Chats: ")
+st.caption("(Write anything)")
+
+chat=st.checkbox("Open chats")
+if chat:
+    prompt = st.chat_input("Say, Hii")
+    if prompt:
+        with st.chat_message("user",avatar="user"):
+            st.write(prompt)
+        with st.chat_message("AI",avatar="assistant"):
+            st.write("Hello 👋")
+   
 st.divider()
 st.subheader("Help :")
 col1_foot,col2_foot = st.columns([3,1])
