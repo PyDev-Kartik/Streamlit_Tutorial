7cf54c8f108151d6a407c1611e9351295e276c8c
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-07 17:13:08 +0530

    Update 1_1 Text Element.py

--- Streamlit/Pages/1_1 Text Element.py
+++ Streamlit/Pages/1_1 Text Element.py
@@ -127,7 +127,7 @@ st.write("###### Hi ")
 
 st.divider()
 st.subheader("Help :")
-col1_foot,col2_foot = st.columns([2,1])
+col1_foot,col2_foot = st.columns([3,1])
 with col1_foot:
         st.page_link("Help.py",label = "Click Here")
 with col2_foot:
