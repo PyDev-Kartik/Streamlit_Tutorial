812ef111fe4f689484eff634120a85e94cccd2ae
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-07 17:15:15 +0530

    Update 4_4 Chart Element.py

--- Streamlit/Pages/4_4 Chart Element.py
+++ Streamlit/Pages/4_4 Chart Element.py
@@ -80,5 +80,10 @@ map_data = pd.DataFrame(
 st.map(map_data,size=50,height=370)
 st.divider()
 st.subheader("Help :")
-st.page_link("Help.py",label = "Click Here")
+col1_foot,col2_foot = st.columns([3,1])
+with col1_foot:
+        st.page_link("Help.py",label = "Click Here")
+with col2_foot:
+        st.page_link("5_5 Form Element.py",label = ":green-background[Next page >]")
 st.divider()
+
