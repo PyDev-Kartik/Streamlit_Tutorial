dfc7d795eb225c54b4c8f3f733e18ca7912d6042
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-07 17:10:37 +0530

    Update 0_0 Initialization.py

--- Streamlit/Pages/0_0 Initialization.py
+++ Streamlit/Pages/0_0 Initialization.py
@@ -75,7 +75,7 @@ st.code("""
 
 st.divider()
 st.subheader("Help :")
-col1_foot,col2_foot = st.columns([2,1])
+col1_foot,col2_foot = st.columns([3,1])
 with col1_foot:
         st.page_link("Help.py",label = "Click Here")
 with col2_foot:
