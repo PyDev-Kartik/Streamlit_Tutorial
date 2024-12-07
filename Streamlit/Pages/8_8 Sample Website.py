f543fe0f20664e7259ca396fbe302835cd73b718
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-07 17:18:13 +0530

    Update 8_8 Sample Website.py

--- Streamlit/Pages/8_8 Sample Website.py
+++ Streamlit/Pages/8_8 Sample Website.py
@@ -1,2 +1,12 @@
 import streamlit as st
 st.title("This page is under constrution.")
+
+st.divider()
+st.subheader("Help :")
+col1_foot,col2_foot = st.columns([3,1])
+with col1_foot:
+        st.page_link("Help.py",label = "Click Here")
+with col2_foot:
+        st.page_link("9_9 Deploy.py",label = ":green-background[Next page >]")
+st.divider()
+
