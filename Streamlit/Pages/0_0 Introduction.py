8f2088af847ea137c88afa47334de8778a0998be
Gecko <121715599+PyDev-Kartik@users.noreply.github.com>
2024-12-08 13:05:43 +0530

    Update 0_0 Introduction.py

--- Streamlit/Pages/0_0 Introduction.py
+++ Streamlit/Pages/0_0 Introduction.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.write("Introduction page")
+
+st.divider()
+st.subheader("Help :")
+col1_foot,col2_foot = st.columns([3,1])
+with col1_foot:
+        st.page_link("Help.py",label = "Click Here")
+with col2_foot:
+        st.page_link("0_0 Introduction.py",label = ":green-background[Next page >]")
+st.divider()
